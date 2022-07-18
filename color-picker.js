034b7ead18f8c063eb285b669177e893fc8027ac
nossair-additio <nossair@additioapp.com>
2022-07-18 16:29:15 +0200

    Canvis per el funcionament del selector de colors.

--- color-picker.js
+++ color-picker.js
@@ -1,54 +1,44 @@
 angular.module('ngColorPicker', [])
-.provider('ngColorPickerConfig', function(){
-    
-    var templateUrl = 'color-picker.html';
-    var defaultColors =  [
-        '#7bd148',
-        '#5484ed',
-        '#a4bdfc',
-        '#46d6db',
-        '#7ae7bf',
-        '#51b749',
-        '#fbd75b',
-        '#ffb878',
-        '#ff887c',
-        '#dc2127',
-        '#dbadff',
-        '#e1e1e1'
-    ];
-    this.setTemplateUrl = function(url){
-        templateUrl = url;
-        return this;
-    };
-    this.setDefaultColors = function(colors){
-        defaultColors = colors;
-        return this;
-    };
-    this.$get = function(){
+    .directive('ngColorPicker', function() {
+        var defaultColors =  [
+            '#7bd148',
+            '#5484ed',
+            '#a4bdfc',
+            '#46d6db',
+            '#7ae7bf',
+            '#51b749',
+            '#fbd75b',
+            '#ffb878',
+            '#ff887c',
+            '#dc2127',
+            '#dbadff',
+            '#e1e1e1'
+        ];
         return {
-            templateUrl : templateUrl,
-            defaultColors: defaultColors
-        }
-    }
-})
-.directive('ngColorPicker', ['ngColorPickerConfig',function(ngColorPickerConfig) {
-    
-    return {
-        scope: {
-            selected: '=',
-            customizedColors: '=colors'
-        },
-        restrict: 'AE',
-        templateUrl: ngColorPickerConfig.templateUrl,
-        link: function (scope, element, attr) {
-            scope.colors = scope.customizedColors || ngColorPickerConfig.defaultColors;
-            scope.selected = scope.selected || scope.colors[0];
+            scope: {
+                selected: '=',
+                customizedColors: '=colors',
+                onColorChange: '&?'
+            },
+            restrict: 'AE',
+            template: '<div ng-include="contentUrl"></div>',
+            link: function (scope, element, attr) {
 
-            scope.pick = function (color) {
-                scope.selected = color;
-            };
+                scope.contentUrl = 'bower_components/ng-color-picker/color-picker.html';
+                if(attr.templateUrl){
+                    scope.contentUrl = attr.templateUrl;
+                }
 
-        }
-    };
+                scope.colors = scope.customizedColors || defaultColors;
+                scope.selected = scope.selected || scope.colors[0];
 
-}]);
+                scope.pick = function (color) {
+                    scope.selected = color;
+                    if(scope.onColorChange){
+                        scope.onColorChange({ color: scope.selected });
+                    }
+                };
+            }
+        };
+
+    });
